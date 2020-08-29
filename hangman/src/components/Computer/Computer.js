18d849a0ed569b41c582dfe284fbd3e94ea40880
teochewthunder <teochewthunder@gmail.com>
2020-08-30 02:47:12 +0800

    Styling

--- hangman/src/components/Computer/Computer.js
+++ hangman/src/components/Computer/Computer.js
@@ -3,9 +3,11 @@ import './Computer.css';
 import Letters from '../Letters';
 
 function Computer() {
+	let message = "loading...";
+
     return (
     	<div className="Computer">
-			<p>Computer</p> 
+			<div className="Message">{message}</div> 
 			<Letters word="computertest" /> 
     	</div> 
     );
