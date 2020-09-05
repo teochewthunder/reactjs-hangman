10253663f95bfb4f8b524eb81f181a86e78347c3
teochewthunder <teochewthunder@gmail.com>
2020-09-05 14:53:42 +0800

    Naming conventons

--- hangman/src/components/Player/Player.js
+++ hangman/src/components/Player/Player.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import './Player.css';
 
 function Player(props) {
-	const [guessWord, setGuessWord] = useState('');	
+	const [guessedWord, setguessedWord] = useState('');	
     const [usedLetters, setUsedLetters] = useState([]);
     
     let playerLetters = 'abcdefghijklmnopqrstuvwxyz'.split('');
@@ -17,7 +17,7 @@ function Player(props) {
 	let mysteryLetters = mysteryWord.split('');
 
 	let BtnConfirm_click = ()=> {
-		if (guessWord === mysteryWord) {
+		if (guessedWord === mysteryWord) {
 			setGuessedLetters(mysteryLetters);
 			setStage(0);
 			setMessage("You Win!");
@@ -45,9 +45,8 @@ function Player(props) {
 		if (mysteryLetters.indexOf(letter) === -1) {
 			setStage(stage + 1);
 		} else {
-			let tempArray = guessedLetters;
-			tempArray.push(letter);
-			setGuessedLetters(tempArray);
+			guessedLetters.push(letter);
+			setGuessedLetters(guessedLetters);
 		}		
 	}
 
@@ -73,11 +72,11 @@ function Player(props) {
 			<div className="Middle">
 				<br /><br />OR
 			</div>
-			<div className="GuessWord">
+			<div className="guessedWord">
 				<h3>Guess The Word</h3>
-				<input type="text" maxLength="13" value={guessWord} onChange={(e)=>{ setGuessWord(RemoveIllegalCharacters(e.target.value)); }}/>
+				<input type="text" maxLength="13" value={guessedWord} onChange={(e)=>{ setguessedWord(RemoveIllegalCharacters(e.target.value)); }}/>
 				<br /><br />
-				<button onClick={()=>{BtnConfirm_click();}} disabled={guessWord.length === 0}>Confirm</button>
+				<button onClick={()=>{BtnConfirm_click();}} disabled={guessedWord.length === 0}>Confirm</button>
 			</div>
     	</div>   
     );
